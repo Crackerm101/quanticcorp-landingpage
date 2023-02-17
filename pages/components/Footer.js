8475a169986a18c57c58dd0e0dec6434d909df60
Jayvee-Friday <91900181+Crackerm101@users.noreply.github.com>
2023-02-17 15:31:49 +0800

    Update Footer.js

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -8,6 +8,9 @@ import {
 } from "react-icons/ri";
 
 export default function Footer() {
+    const today = new Date();
+    const year = today.getFullYear();
+
     return (
         <div className="w-full bg-card-color py-3">
             <div className="container mx-auto flex h-full justify-between px-5 text-title-color">
