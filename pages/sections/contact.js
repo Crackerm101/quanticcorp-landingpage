9d191b09278084f7ce02e69e944a72ef1dbee35f
Jayvee-Friday <91900181+Crackerm101@users.noreply.github.com>
2023-02-17 15:37:04 +0800

    Update contact.js

--- pages/sections/contact.js
+++ pages/sections/contact.js
@@ -4,23 +4,23 @@ function contact() {
     return (
         <div className="conContainer">
             <div className="container mx-auto mt-20 grid grid-cols-1 gap-5 py-20 px-3 md:grid md:grid-cols-2">
-                <div className="flex flex-col justify-center">
+                <div className="flex flex-col justify-center text-center sm:text-start ">
                     <h1
                         className="mb-2 bg-gradient-to-r from-purple-400 to-blue-600 bg-clip-text text-3xl font-black text-transparent 
-           sm:text-center md:text-4xl lg:text-5xl"
+           md:text-4xl lg:text-5xl"
                     >
                         CONTACT US
                     </h1>
-                    <p className="text-2xl font-bold text-title-color sm:text-center">
+                    <p className="text-2xl font-bold text-title-color ">
                         It's Easy to Get Started...
                     </p>
-                    <p className="mt-2 max-w-2xl text-title-color sm:text-center">
+                    <p className="mt-2 max-w-2xl text-title-color ">
                         With our own series of proprietary methodologies and
                         technologies, we help make the results of any effort we
                         take on clear and with a high degree of certainty.
                     </p>
 
-                    <p className="mt-2 max-w-2xl text-title-color sm:text-center  ">
+                    <p className="mt-2 max-w-2xl text-title-color  ">
                         When you are ready, contact us and we will setup a short
                         discovery call and quickly determine if we can help you,
                         and how. A 30 min call could open the door to a huge set
